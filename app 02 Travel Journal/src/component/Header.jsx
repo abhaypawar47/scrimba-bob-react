617dd9f577fbb5e96930675a45527b8eebeb6d14
Abhay Pawar <abhaypawar0407@gmail.com>
2026-01-01 12:04:57 +0530

    header travel journal

---
+++ app 02 Travel Journal/src/component/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <tjHead>
+      <div>
+        <p> 🌐 my travel jonural.</p>
+      </div>
+    </tjHead>
+  );
+}
+
+export default Header;
